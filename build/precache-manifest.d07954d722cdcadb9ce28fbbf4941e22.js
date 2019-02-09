f677c3ce91333dc74909ed43573228a612bd5834
lomas1244 <jameslomas2004@gmail.com>
2019-02-09 11:04:40 +0000

    app files and initial build

---
+++ build/precache-manifest.d07954d722cdcadb9ce28fbbf4941e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e7dbba7890801ea6ecb",
+    "url": "/static/js/main.2e7dbba7.chunk.js"
+  },
+  {
+    "revision": "cb3d881156d53ed6ddd7",
+    "url": "/static/js/1.cb3d8811.chunk.js"
+  },
+  {
+    "revision": "2e7dbba7890801ea6ecb",
+    "url": "/static/css/main.359f5d82.chunk.css"
+  },
+  {
+    "revision": "cb3d881156d53ed6ddd7",
+    "url": "/static/css/1.7ef13e3e.chunk.css"
+  },
+  {
+    "revision": "2ef28eb59aef7e4177de3a1fe02d4279",
+    "url": "/index.html"
+  }
+];
